8a9949f9c789a71e1fac873fd4ddb6c416b5470c
user <ahmadboyabdurahimov589@gmail.com>
2025-02-01 09:19:27 +0500

    add new changes

--- core/settings/base.py
+++ core/settings/base.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = ["*"]
 
 # Application definition
 DJANGO_APPS = [
-    'unfold'
+    'unfold',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
