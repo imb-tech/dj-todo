641f4743018a4dc3fc402e6de593cfad1e48ddf8
user <ahmadboyabdurahimov589@gmail.com>
2025-03-03 18:00:05 +0500

    new changes

--- apps/common/models.py
+++ apps/common/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+
+
+
+
+class Positions(models.TextChoices):
+    BOSS = 'boss', 'Boss'
+    DISPATCHER = 'dispatcher', 'Dispatcher'
+    MANGER = 'manager', 'Manager'
+    OTHER = 'other', 'Other'
+
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Employee(BaseModel):
+    first_name = models.CharField(
+        verbose_name='First Name',
+        max_length=100
+    )
+    last_name = models.CharField(
+        max_length=100,
+        verbose_name='Last Name'
+    )
+    position = models.CharField(
+        verbose_name='Position',
+        max_length=100,
+        choices=Positions.choices
+    )
+
+    class Meta:
+        verbose_name = 'Employee'
+        verbose_name_plural = 'Employees'
+        db_table = 'employees'
+
+
+
+
+
+
