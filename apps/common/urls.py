ec6e3f148b7c3f289aebc548452e350ac25374b9
user <ahmadboyabdurahimov589@gmail.com>
2025-03-04 10:05:41 +0500

    salom

--- apps/common/urls.py
+++ apps/common/urls.py
@@ -1,7 +1,16 @@
-from django.conf.urls import url
+from django.urls import path
+
+
+from . import views
+
+
+
+employee_lc = views.EmployeeViewSet.as_view({'get': 'list', 'post': 'create'})
+employee_udd = views.EmployeeViewSet.as_view({'get': 'retrieve', 'delete': 'destroy', 'patch': 'partial_update'})
 
 urlpatterns = [
-
+    path('employees/', employee_lc, name='employee_lc'),
+    path('employees/<int:pk>/', employee_lc, name='employee_udd'),
 ]
 
 
